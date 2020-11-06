6f636b063c83b998b6eeff7df01c25b5e7f782b1
unknown <manuel.garcia02@uptc.edu.co>
2020-11-05 22:28:27 -0500

    master

---
+++ README.md
@@ -0,0 +1,17 @@
+# react-native-awesome-barcode-scan
+
+## Getting started
+
+`$ npm install react-native-awesome-barcode-scan --save`
+
+### Mostly automatic installation
+
+`$ react-native link react-native-awesome-barcode-scan`
+
+## Usage
+```javascript
+import AwesomeBarcodeScan from 'react-native-awesome-barcode-scan';
+
+// TODO: What to do with the module?
+AwesomeBarcodeScan;
+```
---
+++ android/src/main/java/com/reactlibrary/AwesomeBarcodeScanModule.java
@@ -0,0 +1,61 @@
+package com.reactlibrary;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
+
+import com.facebook.react.bridge.ActivityEventListener;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class AwesomeBarcodeScanModule extends ReactContextBaseJavaModule implements ActivityEventListener {
+
+    private final ReactApplicationContext reactContext;
+    Promise promise;
+    public AwesomeBarcodeScanModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
+
+    @Override
+    public String getName() {
+        return "AwesomeBarcodeScan";
+    }
+
+    @ReactMethod
+    public void scan(Promise promise) {
+        this.promise = promise;
+        ReactApplicationContext context = getReactApplicationContext();
+//        intent = new Intent(context,App.class);
+        context.addActivityEventListener(this);
+//        // TODO: Implement some actually useful functionality
+//        callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
+        IntentIntegrator intentIntegrator = new IntentIntegrator(context.getCurrentActivity());
+        intentIntegrator.initiateScan();
+    }
+
+    @Override
+    public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
+        ReactApplicationContext context = getReactApplicationContext();
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(scanResult != null) {
+            System.out.println(scanResult.toString().replaceAll("[^A-Za-z0-9]+", " "));
+//            Log.i("SCAN", "scan result: " + scanResult.toString().replaceAll("[-!$%^&*()_+|~=`{}\\[\\]:\";'<>?,.\\/]"," ") + "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaoooooooooooooooooooooo");
+            this.promise.resolve(scanResult.toString().replaceAll("[^A-Za-z0-9]+", " "));
+
+        } else
+            Log.e("SCAN", "Sorry, the scan was unsuccessful...");
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+
+    }
+}
